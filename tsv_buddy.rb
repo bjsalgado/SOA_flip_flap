8a0c5c0af20a312ea23254d98336ce190efb820a
bjsalgado <bayardo_salgado@yahoo.com>
2015-10-03 18:05:33 +0800

    tvs_buddy.rb and flip_flap_spec.rb completed

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,33 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    @data= []
+    lines = tsv.split("\n")
+    header = lines[0].chomp("\n").split("\t")
+    lines.delete_at(0)
+    lines.each do  |line|
+      data_hash = {}
+      line.chomp("\n").split("\t").each.with_index do |value, i|
+        data_hash[header[i]] = value
+      end
+      @data << data_hash
+    end
+    @data
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    arr_data = []
+    @data.each.with_index do |data, i|
+      if i == 0
+        arr_data <<  @data[i].keys.join("\t")+"\n"
+        arr_data << data.values.join("\t") + "\n"
+      else
+        arr_data << data.values.join("\t") + "\n"
+      end
+    end
+    arr_data.join
   end
+
 end
